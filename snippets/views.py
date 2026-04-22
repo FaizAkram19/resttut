f25c62aea11b5868b022e7d0a10d335bbe857c9a
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-04-22 15:24:08 +0000

    Replaced Class Based Views with mixins

--- snippets/views.py
+++ snippets/views.py
@@ -7,9 +7,14 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 
 
 # Create your views here.
+
+#Function Based Views
+
 """
 @csrf_exempt
 
@@ -55,6 +60,9 @@ def snippet_detail(request, pk, format=None):
      
 """
 
+#Class Based Views
+
+"""
 class SnippetList(APIView):
     def get(self, request, format=None):
         snippets=Snippet.objects.all()
@@ -86,4 +94,36 @@ class SnippetDetail(APIView):
         snippet=self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+"""
+
+
+#using mixins
+
+class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset=Snippet.objects.all()
+    serializer_class=SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
     
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class SnippetDetail(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView
+):
+    queryset=Snippet.objects.all()
+    serializer_class=SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
